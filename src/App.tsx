1589f672790380af7ee294d9086e00ceaf726acf
hanyugeon <hanyugeon@naver.com>
2022-05-14 13:26:07 +0900

    Feat: ChakraUi, react-dom(v6), web3.js를 활용한 프론트엔드 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./routes/main";
 
 const App: FC = () => {
+  const [account, setAccount] = useState<String>("");
+  
+  const getAccount = async () => {
+    try {
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({
+          method: 'eth_requestAccounts',
+        });
+
+        setAccount(accounts[0]);
+      } else {
+        alert("Install Metamask")
+      }
+    } catch(error) {
+      console.error(error)
+    }
+  }
+  
+  useEffect(() => {
+    getAccount();
+  }, []);
+
+  useEffect(() => {
+    console.log(account);
+  }, [account]);
+
   return (
     <BrowserRouter>
       <Routes>
